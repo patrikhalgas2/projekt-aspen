11660d3183eb566d5ff02457f28cba64bc0bc6e8
Patrik Halgaš <66626683+patrikhalgas2@users.noreply.github.com>
2021-01-08 16:23:04 +0100

    Add files via upload

---
+++ oldjavascript/old.js
@@ -0,0 +1,71 @@
+
+		// plays the svg animation in the given object element
+		function playSvg(objEl)
+		{
+			if (objEl.contentDocument && objEl.contentDocument.defaultView.KeyshapeJS) {
+				var ks = objEl.contentDocument.defaultView.KeyshapeJS;
+				if (ks.globalState() != "running") {
+					ks.globalPlay();
+				}
+			}
+		}
+		 
+		// pauses the svg animation in the given object element
+		function pauseSvg(objEl)
+		{
+			if (objEl.contentDocument && objEl.contentDocument.defaultView.KeyshapeJS) {
+				var ks = objEl.contentDocument.defaultView.KeyshapeJS;
+				if (ks.globalState() != "idle" && ks.globalState() != "paused") {
+					ks.globalPause();
+				}
+			}
+		}
+		
+		
+		
+
+
+
+			// checks if the element is visible, only completely visible elements return true
+			function isInView(objEl) {
+				var margin = -200;
+				var rect = objEl.getBoundingClientRect();
+				return rect.top >= margin && rect.bottom <= window.innerHeight-margin;
+			}
+			
+
+
+
+				// starts playing animation if it is immediately visible
+				function playSvgIfInView(objEl)
+				{
+					if (isInView(objEl)) {
+						objEl.contentDocument.defaultView.KeyshapeJS.globalPlay();
+					}
+				}
+
+
+
+			window.addEventListener("scroll", function() {
+				var els = document.querySelectorAll(".scroll-activated");
+				for (var i = 0; i < els.length; i++) {
+					var el = els[i];
+					if (isInView(el)) {
+						playSvg(el);
+					} else {
+						pauseSvg(el);
+					}
+				}
+			}, false);
+
+/*-------Preloader--------*/
+			document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector("body").style.visibility = "hidden"; 
+        document.querySelector("#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector("#loader").style.display = "none"; 
+        document.querySelector("body").style.visibility = "visible"; 
+    } 
+}; 
+			
