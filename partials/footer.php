3f4e97913b16145c181203d8bfa83af4baf890fb
Patrik Halgaš <66626683+patrikhalgas2@users.noreply.github.com>
2021-01-08 16:21:28 +0100

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,120 @@
+</main>	
+	
+	
+
+	<footer>
+		<div class="mobile-footer-menu container-fluid d-lg-none">
+			<div class="row">
+					<a class="col-4 background-white "  href="#top" >
+						<img src="/img/logo-aspen-ikona-2.svg" alt="">
+					</a>
+					<a class="col-4 background-red customScroll" name="#section-client">
+						<p class="text-center  m-auto">For Client </p>
+					</a>
+					<a class="col-4 background-black customScroll" name="#section-candidate">
+						<p class="text-center  m-auto ">For Candidate</p>
+					</a>
+			</div>
+		</div>
+
+		<a id="back-to-top" href="#" class="text-danger back-to-top" role="button">
+			<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-arrow-up-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+				<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+				<path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+		  	</svg>
+		</a>
+
+		<div class="container-fluid " >
+            <div class="row">
+				<div class="col-7 col-md-3 col-lg-3 col-xl-2 ml-5 mt-lg-4">
+					<a href="/index.php"><img class="img-fluid " src="/img/logo.svg"></img></a>
+				</div>
+		        <div class="footer-cut col-6 col-md-4 col-lg-2 offset-xl-1">
+				    <h2>Navigation</h2>
+				        <ul>
+					        <li><a href="/index.php">Home</a></li>
+							<li><a href="/services.php">Services</a></li>
+							<li><a href="/about-us.php">About Us</a></li>
+							<li><a href="/candidate-section.php">Candidate Section</a></li>
+							<li><a href="/ethical-code.php">Our Ethical Code</a></li>
+					        <li><a href="/contact.php">Contact</a></li>	
+				        </ul>
+			    </div>
+			    <div class="footer-cut col-6 col-md-4 col-lg-2">
+				    <h2>Useful Links</h2>
+				    <ul>
+        			    <li><a href="/we-think-green.php">We Think Green</a></li>
+					    <li><a href="/privacy-policy.php">Privacy Policy</a></li>
+					    <li><a href="/cookies-and-gdpr.php">Cookies & GDPR</a></li>	
+				    </ul>
+			    </div>
+			    <div class="footer-cut col-12 col-md-3 col-lg-4 mb-sm-5 mb-md-0 ">
+				    <h2>Contact</h2>
+				    <ul>
+					    <li><p class="p-red">office:<a class="hover-footer" href="https://www.google.com/maps/@48.1445882,17.1051299,3a,75y,285.56h,104.41t/data=!3m6!1e1!3m4!1sN3jUq2HWydXaIEn8pcTQCw!2e0!7i13312!8i6656?hl=en"> AspenStaff, s.r.o., Klariská 7 , 811 03 Bratislava, Slovak Republic</a></p></li>
+        			    <li><p class="p-red">tel.:<a class="hover-footer" href="tel:+421254644480"> +421254644480</a></p></li>
+					    <li><p class="p-red">email:<a class="hover-footer" href="mailto:info@aspen-staff.com"> info@aspen-staff.com</a></p></li>
+				    </ul>
+                 </div>
+            </div>
+		</div>	
+	</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	
+	<!-- WOW JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+			  
+			 
+			 <script>
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false       // trigger animations on mobile devices (true is default)
+  }
+);
+wow.init();
+			  </script>
+			
+	
+	<!--slow scrolling -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="/oldjavascript/scroll.js"></script>
+
+	<!-- Old JavaScript -->
+	<script src="/oldjavascript/old.js"></script>
+
+	<!-- mobile -->
+	<script src="/oldjavascript/mobile.js"></script>
+
+	
+	<!-- preloader -->
+	<script> 
+        document.onreadystatechange = function() { 
+            if (document.readyState !== "complete") { 
+                document.querySelector( 
+                  "body").style.visibility = "hidden"; 
+                document.querySelector( 
+                  "#loader").style.visibility = "visible"; 
+            } else { 
+                document.querySelector( 
+                  "#loader").style.display = "none"; 
+                document.querySelector( 
+                  "body").style.visibility = "visible"; 
+            } 
+        }; 
+    </script>
+<script src="/js/test1.js"></script>
+<script src="/js/recruit.js"></script>
+<script src="/js/jquery.cookie.js"></script>
+<script src="/js/mails.js"></script>
+<script src="/js/cookies.js"></script>
+
+  </body>
+</html>
